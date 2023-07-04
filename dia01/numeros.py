ce67be86de7620dea73b4b6d7c92bb5d3bb3da45
Téo Calvo <teocalvo2@gmail.com>
2023-07-03 21:49:20 -0300

    Dia 01

---
+++ dia01/numeros.py
@@ -0,0 +1,28 @@
+# %%
+print("Soma 10+1 =", 10+1)
+
+# %%
+20 + 30
+
+# %%
+20 * 3
+
+# %%
+print(1 + 1) # 2
+print(2 * 3) # 6
+
+# %%
+print("Subtração: 10 - 5 =", 10 - 5)
+
+# %% 
+print("Resto da divisão 3 por 2 =", 3 % 2)
+
+# %%
+print("Expoente: 10 ^ 2 =", 10 ** 2)
+
+# %%
+print("Divisão: 10 / 5 =", 10 / 5)
+
+# %%
+print("Divisão: 10 / 5 =", 10 // 5)
+print("Divisão: 3 / 2 =", 3 // 2)
---
+++ dia01/variaveis.py
@@ -0,0 +1,20 @@
+# %%
+soma = 10 + 20
+
+soma = 2 * 3
+
+print(soma)
+
+# %%
+
+nome = input("Entre com o seu nome: ")
+print("Olá, ", nome, ". Você é bem vindo(a)!", sep="")
+
+# %%
+
+a = input("Entre com o primeiro número: ")
+a = int(a)
+
+b = int(input("Entre com o segundo número: "))
+
+print(a, "+" , b ,"=", a+b)
